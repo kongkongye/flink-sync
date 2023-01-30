ab13306053c3dad49e308874e9b0b0a8d99a9414
kongkongye <kongkongye@163.com>
2023-01-30 10:06:43 +0800

    date converter

--- src/main/java/com/kongkongye/flink/sync/table/converter/common/DateConverter.java
+++ src/main/java/com/kongkongye/flink/sync/table/converter/common/DateConverter.java
@@ -18,7 +18,7 @@ public class DateConverter implements Converter<Object, String> {
 
     @Override
     public boolean canHandle(String dataType, Object value) {
-        return "date".equalsIgnoreCase(dataType);
+        return "datetime".equalsIgnoreCase(dataType);
     }
 
     @Override
