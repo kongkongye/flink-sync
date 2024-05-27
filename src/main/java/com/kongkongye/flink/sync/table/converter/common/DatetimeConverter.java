9d829fe6e78cce6f9b6b55f2d3f1d568023edd63
kongkongye <kongkongye@163.com>
2024-05-27 15:29:52 +0800

    fix DatetimeConverter

--- src/main/java/com/kongkongye/flink/sync/table/converter/common/DatetimeConverter.java
+++ src/main/java/com/kongkongye/flink/sync/table/converter/common/DatetimeConverter.java
@@ -10,7 +10,7 @@ import java.util.TimeZone;
 /**
  * ms时间戳 => "yyyy-MM-dd HH:mm:ss.SSS"
  */
-public class DatetimeConverter implements Converter<Long, String> {
+public class DatetimeConverter implements Converter<Number, String> {
     @Override
     public String name() {
         return "datetime";
@@ -18,18 +18,18 @@ public class DatetimeConverter implements Converter<Long, String> {
 
     @Override
     public boolean canHandle(String dataType, Object value) {
-        return value instanceof Long && ("datetime".equalsIgnoreCase(dataType) || "timestamp".equalsIgnoreCase(dataType));
+        return value instanceof Number && ("datetime".equalsIgnoreCase(dataType) || "timestamp".equalsIgnoreCase(dataType));
     }
 
     @Override
-    public String convert(JSONObject config, Long input) {
+    public String convert(JSONObject config, Number input) {
         //config
         long offset = config.getLongValue("offset", 0L);//偏移，可为负，目的是将时间调整为标准的utc+0时间，单位ms
         String timezone = config.getString("timezone");//时区
 
         TimeZone timeZone = TimeZone.getTimeZone(timezone);
         Calendar calendar = Calendar.getInstance(timeZone);
-        calendar.setTimeInMillis(input+offset);
+        calendar.setTimeInMillis(input.longValue()+offset);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         return sdf.format(calendar.getTime());
     }
